73620df31640ac041ffb12d101c38ced77eb5144
NTiger07 <falaleru@gmail.com>
2023-08-22 20:33:51 +0100

    1768 done

--- Arrays/.py
+++ Arrays/.py
@@ -1,10 +1,16 @@
 word1 = "stew"
-word2 = "ls"
+word2 = "list"
 
-
-
-
-space_string = "t" *(len(word1) - len(word2))
+space_string = " " *(len(word1) - len(word2))
 word2 += space_string
+string = ""
 
-print(word2)
+for i in range(len(word1)):
+    string += word1[i]
+    string += word2[i]
+    
+
+print(string)
+    
+if !char:
+    print("crud")
--- Arrays/1768.MergeStringsAlt.py
+++ Arrays/1768.MergeStringsAlt.py
@@ -1,18 +1,26 @@
 def mergeAlternately(word1: str, word2: str) -> str:
+        string = ""
+        
         if len(word1) == len(word2):
             for i in range(len(word1)):
-                print(word1[i], end="")
-                print(word2[i], end="")
+                string += word1[i]
+                string += word2[i]
+            
         elif len(word1) > len(word2):
             word2 += " " *(len(word1) - len(word2))
             for i in range(len(word1)):
-                    print(word1[i], end="")
-                    print(word2[i], end="")
+                    string += word1[i]
+                    string += word2[i]
+                  
         else:
             word1 += " " *(len(word2) - len(word1))
             for i in range(len(word1)):
-                    print(word1[i], end="")
-                    print(word2[i], end="")
+                    string += word1[i]
+                    string += word2[i]
+        
+        string = string.replace(" ", "")
+        return string
+        
                 
-mergeAlternately("stop", "lift")                
+print(mergeAlternately("stop", "li"))            
                 
