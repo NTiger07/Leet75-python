eca8b0e3cf6712c0ac53fc8b1ce5ef778edbf686
NTiger07 <falaleru@gmail.com>
2023-08-22 19:52:23 +0100

    almost done

---
+++ Arrays/.py
@@ -0,0 +1,10 @@
+word1 = "stew"
+word2 = "ls"
+
+
+
+
+space_string = "t" *(len(word1) - len(word2))
+word2 += space_string
+
+print(word2)
--- Arrays/1768.MergeStringsAlt.py
+++ Arrays/1768.MergeStringsAlt.py
@@ -1,3 +1,18 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        if len(word1) == len(word2)
+def mergeAlternately(word1: str, word2: str) -> str:
+        if len(word1) == len(word2):
+            for i in range(len(word1)):
+                print(word1[i], end="")
+                print(word2[i], end="")
+        elif len(word1) > len(word2):
+            word2 += " " *(len(word1) - len(word2))
+            for i in range(len(word1)):
+                    print(word1[i], end="")
+                    print(word2[i], end="")
+        else:
+            word1 += " " *(len(word2) - len(word1))
+            for i in range(len(word1)):
+                    print(word1[i], end="")
+                    print(word2[i], end="")
+                
+mergeAlternately("stop", "lift")                
+                
