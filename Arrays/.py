267f36b8605facbff8fb365e274270ab25b37732
NTiger07 <falaleru@gmail.com>
2023-08-24 00:04:22 +0100

    testing...*

--- Arrays/.py
+++ Arrays/.py
@@ -1,6 +1,5 @@
 str1 = "ABABAB"
 str2 = "ABAB"
-# expect AB SINCE ABAB APPEAR ONLY ONCE IN STR1
 
 if max(len(str1), len(str2)) == len(str1):
     long = str1
@@ -16,11 +15,17 @@ for i in range(len(long)):
     if str1[i] == str1[0] and i != 0:
         res += str1[:i]
         break
-    
 
-# if res in short:
-#     numberOccur += short.count(res)
+if res in short:
+    numberOccur += short.count(res)
 
-print(res)
-       
-# print(res*numberOccur)
+
+# Check how many times AB is in long and check how many times ABAB is in long and choose max
+
+resOccursInLong = long.count(res)
+resOccursInLong2 = long.count(res*numberOccur)
+
+if resOccursInLong > resOccursInLong2:
+    print(res)
+else:
+    print(res*numberOccur)
