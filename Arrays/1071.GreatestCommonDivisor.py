c3adb8d378891a6064e64287e9f9e81a249dd818
NTiger07 <falaleru@gmail.com>
2023-08-23 12:02:00 +0100

    testing...

--- Arrays/1071.GreatestCommonDivisor.py
+++ Arrays/1071.GreatestCommonDivisor.py
@@ -2,19 +2,34 @@ def gcdOfStrings(str1: str, str2: str):
     str1 = set(str1)
     str2 = set(str2)
     
-    res1 = ""
-    res2 = ""
+    res1 = []
+    res2 = []
+    
+    str3=""
+    str4=""
 
+    
     for char in str1:
-        res1 += char
+        res1.append(char)
         
     for char in str2:
-        res2 += char
+        res2.append(char)
         
-    if res1 in res2:
-        print(res1)
+    res1.sort()
+    res2.sort()
+    
+    for char in res1:
+        str3 += char
+        
+    for char in res2:
+        str4 += char
+    
+    if str3 in str4:
+        return str3
     else:
-        print("")
+        return '""'
  
  
-print(gcdOfStrings("ABCABC", "ABC"))
+print(gcdOfStrings("LEET", "CODE"))
+print(gcdOfStrings("ABCABCABC", "ABC"))
+print(gcdOfStrings("ABABAB", "AB"))
