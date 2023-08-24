335e35fe29ad1fd3912ffc1b571797aab10c414f
NTiger07 <falaleru@gmail.com>
2023-08-24 13:29:54 +0100

    new approach worked!

--- Arrays/.py
+++ Arrays/.py
@@ -1,7 +1,12 @@
 str1 = "TAUXXTAUXXTAUXXTAUXXTAUXX"
 str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"
 from math import gcd
+
+
+gCD = gcd(len(str1), len(str2))
+
+print(gCD)
 if (str1 + str2) == (str2 + str1):
-    print("Equal")
+    print(str1[:gCD])
 else:
     print("")
--- Arrays/1071.GreatestCommonDivisor.py
+++ Arrays/1071.GreatestCommonDivisor.py
@@ -1,34 +1,7 @@
-def gcdOfStrings(str1: str, str2: str):
-    str1 = set(str1)
-    str2 = set(str2)
-    
-    arr1 = []
-    arr2 = []
-    
-    str3 = ""
-    str4 = ""
-    
-    for char in str1:
-        arr1.append(char)
-        
-    for char in str2:
-        arr2.append(char)
-        
-    arr1.sort()
-    arr2.sort()
-    
-    for char in arr1:
-        str3 += char
-        
-    for char in arr2:
-        str4 += char
-    
-    if str3 in str4:
-        return str3
-    else:
-        return 'crud'
- 
- 
-# print(gcdOfStrings("LEET", "CODE"))
-print(gcdOfStrings("ABCABC", "ABC"))
-# print(gcdOfStrings("ABABAB", "AB"))
+
+from math import gcd
+def gcdOfStrings(self, str1: str, str2: str) -> str:    
+    if (str1 + str2) == (str2 + str1):
+        return str1[:gcd(len(str1), len(str2))]
+    return ""
+       
