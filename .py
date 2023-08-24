5b1e3e04e4dfdcfa9b47755a1d5d9a3788c945c5
NTiger07 <falaleru@gmail.com>
2023-08-24 15:08:17 +0100

    605 start

--- .py
+++
@@ -1,6 +0,0 @@
-candies = [2,3,5,1,3]
-
-i = 0
-while i < len(candies):
-    candies[i] 
-    i += 1
---
+++ Arrays/605.PlaceFlowers.py
@@ -0,0 +1,2 @@
+def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+    print("Hello world")
