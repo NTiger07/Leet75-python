c39b6b1a41cd02e81cdaeebe99a7e788f2facd96
NTiger07 <falaleru@gmail.com>
2023-08-24 13:31:04 +0100

    added metrics

--- Arrays/1071.GreatestCommonDivisor.py
+++ Arrays/1071.GreatestCommonDivisor.py
@@ -4,4 +4,7 @@ def gcdOfStrings(self, str1: str, str2: str) -> str:
     if (str1 + str2) == (str2 + str1):
         return str1[:gcd(len(str1), len(str2))]
     return ""
+
+# Runtime 33ms Beats 94.97% of users with Python3
+# Memory 16.20MB Beats 74.83%of users with Python3
        
