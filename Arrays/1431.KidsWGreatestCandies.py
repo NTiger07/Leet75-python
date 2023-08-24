c8f75bc2e822bf5a20411c4ef9f3ac34f50b7af3
NTiger07 <falaleru@gmail.com>
2023-08-24 14:58:29 +0100

    added metrics

--- Arrays/1431.KidsWGreatestCandies.py
+++ Arrays/1431.KidsWGreatestCandies.py
@@ -8,3 +8,7 @@ def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
             res.append(candies[i] >= max_candies)  # Append True if candies[i] is greater or equal to max_candies, False otherwise
                 
         return res
+    
+    
+# Runtime 37ms Beats 96.51% of users with Python3
+# Memory 16.40MB Beats 33.34% of users with Python3
