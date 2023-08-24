d0180c6d6adde754c38a8e31e0948bdb3d7c4f65
NTiger07 <falaleru@gmail.com>
2023-08-24 13:19:16 +0100

    trying new approach

--- Arrays/.py
+++ Arrays/.py
@@ -1,31 +1,7 @@
-str1 = "ABABAB"
-str2 = "ABAB"
-
-if max(len(str1), len(str2)) == len(str1):
-    long = str1
-    short = str2
+str1 = "TAUXXTAUXXTAUXXTAUXXTAUXX"
+str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"
+from math import gcd
+if (str1 + str2) == (str2 + str1):
+    print("Equal")
 else:
-    long = str2
-    short = str1
-
-res = ""
-numberOccur = 0
- 
-for i in range(len(long)):
-    if str1[i] == str1[0] and i != 0:
-        res += str1[:i]
-        break
-
-if res in short:
-    numberOccur += short.count(res)
-
-
-# Check how many times AB is in long and check how many times ABAB is in long and choose max
-
-resOccursInLong = long.count(res)
-resOccursInLong2 = long.count(res*numberOccur)
-
-if resOccursInLong > resOccursInLong2:
-    print(res)
-else:
-    print(res*numberOccur)
+    print("")
