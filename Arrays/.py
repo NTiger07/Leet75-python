44d12a9179117110643338b77c79d71852b6969a
NTiger07 <falaleru@gmail.com>
2023-08-25 13:51:30 +0100

    progress...

--- Arrays/.py
+++ Arrays/.py
@@ -1,3 +1,16 @@
+flowerbed = [1,0,0,0,1]
+n = 1
+
+freeSpaces = flowerbed.count(0)
+
+res = []
+
+for i in range(len(flowerbed)):
+    if flowerbed[i] == 0:
+        res.append(i)
+        
+# print(freeSpaces)
+print(res)
 
 
 
