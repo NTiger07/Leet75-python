7c6b020f8c1ad1bdccd9da8d22bb34b88fa7c8d3
NTiger07 <falaleru@gmail.com>
2023-08-26 19:01:27 +0100

    getting postiton of vowels

--- Arrays/.py
+++ Arrays/.py
@@ -1,2 +1,10 @@
+vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 
-    
+str = "hello"
+positionOfVowels = []
+
+for i in range(len(str)):
+    if str[i] in vowels:
+        positionOfVowels.append(i)
+        
+print(positionOfVowels)
