722fec07e7f6838223b2fef1c0dc5b8e31a5bef1
NTiger07 <falaleru@gmail.com>
2023-08-28 01:45:07 +0100

    algo works!!!

--- Arrays/345.ReverseVowels.py
+++ Arrays/345.ReverseVowels.py
@@ -1,2 +1,27 @@
 def reverseVowels(s:str) -> str:
-    vowels = ["a", "e", "i", "o", "u"]
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+
+    string = "hello"
+    values = []
+    indexes = []
+    res = []
+    resString = ''
+
+    strArray = list(string)
+        
+    for char in strArray:
+        if char not in vowels:
+            res.append(char)
+        
+    for index, value in enumerate(strArray):
+        if value in vowels:
+            indexes.append(index)
+            values.insert(-1, value)
+
+    for i in range(len(indexes)):
+        res.insert(indexes[i], values[i])
+        
+    return resString.join(res)
+
+
+print(reverseVowels("hello"))
