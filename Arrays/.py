5ee15c6800dc9192946fdccd7a4bfb799340f44f
NTiger07 <falaleru@gmail.com>
2023-08-29 14:24:36 +0100

    using two pointers approach

--- Arrays/.py
+++ Arrays/.py
@@ -1,32 +1,20 @@
-vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
 
 string = "race car"
 string = list(string)
-values = []
-indexes = []
-    
 
-# for char in string:
-#     if char not in vowels:
-#         res.append(char)
-    
-# for index, value in enumerate(string):
-#     if value in vowels:
-#         indexes.append(index)
-#         values.insert(-1, value)
+l = 0
+r = len(string) - 1
 
-# for i in range(len(indexes)):
-#     # res.insert(indexes[i], values[i])
-#     string[index[i]] = values[i]
-
-for i in range(len(string)):
-    if string[i] in vowels:
-        values.append(string[i])
-        indexes.append(i)
-values = values[::-1]
-for i in range(len(indexes)):
-    string[indexes[i]] = values[i]
-
+while (l < r):
+    if string[l] not in vowels:
+        l += 1
+    elif string[r] not in vowels:
+        r -= 1
+    else:
+        string[l], string[r] = string[r], string[l]
+        l += 1
+        r -= 1
     
 
 print("".join(string))
