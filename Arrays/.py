15b719bda833f6338c00dcb4ef4206aee348b519
NTiger07 <falaleru@gmail.com>
2023-08-30 00:56:09 +0100

    algo works for major testcase

--- Arrays/.py
+++ Arrays/.py
@@ -4,14 +4,15 @@ words = []
 l = 0
 r = 0
 
+string = string + " "
 while r < len(string):
     if string[r] == " ":
-        words.append(string[l:r])
+        words.append(string[l:r+1])
         l = r + 1
         r += 1
 
     elif string[r] != " ":
         r += 1
-       
-        
-print(words)
+    
+res = "".join(words[::-1])
+print(res.strip())
