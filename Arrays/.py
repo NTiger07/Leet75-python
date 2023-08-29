f57851c89dc84f7f43fedd63199e3dbd1b8a6375
NTiger07 <falaleru@gmail.com>
2023-08-29 23:22:48 +0100

    logic fixes

--- Arrays/.py
+++ Arrays/.py
@@ -6,8 +6,8 @@ r = 0
 
 while r < len(string):
     if string[r] == " ":
-        words.append(string[l:r+1])
-        l = r
+        words.append(string[l:r])
+        l = r + 1
         r += 1
 
     elif string[r] != " ":
