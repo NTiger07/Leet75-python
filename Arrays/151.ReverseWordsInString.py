645c2cce1f1dc02b4a2e180f0bc3ca1e0558ecc8
NTiger07 <falaleru@gmail.com>
2023-08-30 01:30:56 +0100

    testing...

--- Arrays/151.ReverseWordsInString.py
+++ Arrays/151.ReverseWordsInString.py
@@ -1,5 +1,24 @@
 def reverseWords(s: str) -> str:
-    print(s)
-    
+    words = []
+
+    l = 0
+    r = 0
+    s = s + " "
+    while r < len(s):
+        if s[r] == " ":
+            words.append(s[l:r+1])
+            l = r + 1
+            r += 1
+
+        elif s[r] != " ":
+            r += 1
+        
+    if " " in words:
+        words.remove(" ")
+        
+    return "".join(words[::-1]).strip()
+
     
 print(reverseWords("the sky is blue"))
+print(reverseWords(" hello world "))
+print(reverseWords("a good  example"))
