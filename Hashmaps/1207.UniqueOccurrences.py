f5c8ce204c90c89d348e9f4d06ccb7c83a65665a
NTiger07 <falaleru@gmai.com>
2023-09-23 20:18:00 +0100

    Added Metrics

--- Hashmaps/1207.UniqueOccurrences.py
+++ Hashmaps/1207.UniqueOccurrences.py
@@ -12,3 +12,6 @@ def uniqueOccurrences(arr: list[int]) -> bool:
     occurrences = hash.values()
 
     return len(occurrences) == len(set(occurrences))
+
+# Runtime 42ms Beats 75.60% of users with Python3
+# Memory 16.37MB Beats 77.08% of users with Python3
