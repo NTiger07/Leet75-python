2c2fb0b4c49417fbae23a3ce983f5549381c9327
NTiger07 <falaleru@gmai.com>
2023-09-23 20:19:50 +0100

    Added Metrics for OneLiner

--- Hashmaps/1207.OneLiner.py
+++ Hashmaps/1207.OneLiner.py
@@ -5,4 +5,6 @@ from collections import Counter
 def uniqueOccurrences(arr: list[int]) -> bool:
     return len(set(Counter(arr).values())) == len(Counter(arr).values())
 
-print(uniqueOccurrences(arr))
+
+# Runtime 43ms Beats 65.55% of users with Python3
+# Memory1 6.37MB Beats 77.08% of users with Python3
